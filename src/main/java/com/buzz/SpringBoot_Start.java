021144c52d8e6167cf980ae3111470d34df55ab3
421246627 <421246627@qq.com>
2018-09-25 15:51:27 +0800

    嗡嗡嗡旅游网项目

---
+++ src/main/java/com/buzz/SpringBoot_Start.java
@@ -0,0 +1,13 @@
+package com.buzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot_Start
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBoot_Start.class,args);
+    }
+}
