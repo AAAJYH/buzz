5a38b63c9d41b24584a7a9d28d3f16dea26d3072
aaajyh <1514324415@qq.com>
2018-10-06 10:49:38 +0800

    Merge branch 'master' of https://github.com/421246627/buzz into zhf
                                                    # Conflicts:
                                                    #	src/main/java/com/buzz/controller/TestController.java
                                                    #	src/main/resources/templates/ScenicSpot.html

--- src/main/java/com/buzz/controller/hotelController.java
+++ src/main/java/com/buzz/controller/hotelController.java
@@ -1,7 +1,15 @@
 package com.buzz.controller;
 
+import com.buzz.entity.city;
+import com.buzz.entity.scenicspot;
+import com.buzz.service.scenicspotService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @Author: aaaJYH
@@ -13,10 +21,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/hotelController")
 public class hotelController {
 
+    @Resource
+    scenicspotService scenicspotService;
 
-    //酒店页面
+    /**
+     * 查看城市酒店
+     * @param model 景点集合
+     * @param request 获取ServletContext对象
+     * @param scenicspotId 默认选中的id
+     * @return 酒店页面
+     */
     @RequestMapping("/hotelIndex")
-    public String hotelIndex(){
+    public String hotelIndex(Model model, HttpServletRequest request,String scenicspotId){
+        //获取上下文city对象
+        city city=(city) request.getServletContext().getAttribute("city");
+        //保存景点集合
+        List<scenicspot> scenicspotList=scenicspotService.byCityIdQueryScenicspot(city.getCityId());
+        model.addAttribute("scenicspotList",scenicspotList);
+        //默认选中第一个景点
+        if (scenicspotId .equals("")) {
+            scenicspotId=scenicspotList.get(0).getScenicSpotId();
+        }
+        scenicspot scenicspot=scenicspotService.byScenicSpotIdQueryScenicSpot(scenicspotId);
+        model.addAttribute("scenicspot",scenicspot);
         return "front_desk/Hotel";
     }
 
